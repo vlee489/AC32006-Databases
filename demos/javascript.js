3a4190155c676b6d7e192de3ccc52828813d892f
Max Kelly <maxwill.kelly@gmail.com>
2020-11-11 22:24:55 +0000

    Adds demos for javascript tutorial

---
+++ demos/javascript.js
@@ -0,0 +1,3 @@
+/*
+    My favourite cheatsheat: https://www.codecademy.com/learn/introduction-to-javascript/modules/learn-javascript-introduction/cheatsheet
+*/
---
+++ demos/react-calculator/components/Header.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import React from "react";
+
+const Header = () => (
+    <Head>
+        <title>React Calculator</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+    </Head>
+)
+
+export default Header
