ad9da08e4ee789cdf2c62656bf5c0b3b843b7346
Vincent Lee <vlee@vlee.me.uk>
2020-11-12 19:23:02 +0000

    Initial working GraphQL DB connection

--- server/src/db.js
+++ server/src/db.js
@@ -1,6 +1,4 @@
 const Knex = require('knex');
 const knexConfig = require('./knexfile');
 const { Model } = require('objection');
-
-const knex = Knex(knexConfig.development);
-module.exports = Model.knex(knex);
+module.exports = Knex(knexConfig.development);
--- server/src/index.js
+++ server/src/index.js
@@ -1,41 +1,53 @@
 // Main file to runs everything
 const { ApolloServer, gql } = require('apollo-server');
+const { Model } = require('objection');
+const db  = require('./db')
+const { Products } = require('./models/products')
+
+Model.knex(db);
+
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-
   type Product{
-      
+      ProductID: ID
+      Name: String
+      Catergory: Int
+      Price: Float
+      Description: String
+      Weight: Float
+      Colour: String
+      Dimensions: String
   }
 
+  type Query{
+    getProducts(ProductID: ID, Catergory: Int): [Product]
+  }
 `;
-
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
-    Query: {
-      books: () => books,
-    },
-  };
+  Query: {
+    getProducts: (parent, arg, ctx, info) => {
+      dbQuery = Products.query();
 
-  // The ApolloServer constructor requires two parameters: your schema
+      if ('ProductID' in arg){
+        dbQuery = dbQuery.where('ProductID', arg.ProductID);
+      }
+
+      if ('Catergory' in arg){
+        dbQuery = dbQuery.where('Catergory', arg.Catergory);
+      }
+
+      return dbQuery;
+    },
+  },
+};
+
+  // The ApoclloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
 
