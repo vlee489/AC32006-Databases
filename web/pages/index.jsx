8674edb1031714d7609c53a323aa3c08fcf671ae
Nick <n.devilliers1999@gmail.com>
2020-11-13 14:51:06 +0000

    Tried and failed to vert center the stupid fucking login card.

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -1,8 +1,7 @@
 import Head from 'next/head'
-import styles from '../styles/Login.module.scss'
-import { Button, Container, Card, Row, Col, Form } from "react-bootstrap";
+import styles from '../styles/Browse.module.scss'
 
-export default function Login() {
+export default function Browse() {
   return (
     <div className={styles.container}>
       <Head>
@@ -11,34 +10,6 @@ export default function Login() {
       </Head>
 
       <main className={styles.main}>
-        <Container>
-          <Row className="">
-            <Col>
-              <Card className="login-card align-middle">
-                <Card.Body>
-                  <Card.Title className="text-center">Login</Card.Title>
-                  <Form>
-                    <Form.Group controlId="formBasicEmail">
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control type="email" placeholder="Enter email" />
-                      <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                      </Form.Text>
-                    </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                      <Form.Check type="checkbox" label="Stay signed in" />
-                    </Form.Group>
-                  </Form>
-                  <Button variant="primary">Sign in</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
       </main>
     </div>
   )
---
+++ web/pages/staff/login.jsx
@@ -0,0 +1,46 @@
+import Head from 'next/head'
+import styles from '../../styles/staff/Login.module.scss'
+import { Button, Container, Card, Row, Col, Form } from "react-bootstrap";
+
+export default function Login() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Login</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <Container className={styles.container}>
+          {/* <Row>
+            <Col> */}
+              <Card className={styles.loginCard}>
+                <Card.Body>
+                  <Card.Title className="text-center">Login</Card.Title>
+                  <Form>
+                    <Form.Group controlId="formBasicEmail">
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control type="email" placeholder="Enter email" />
+                      <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                          </Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                      <Form.Check type="checkbox" label="Stay signed in" />
+                    </Form.Group>
+                  </Form>
+                  <Button variant="primary">Sign in</Button>
+                </Card.Body>
+              </Card>
+            {/* </Col>
+          </Row> */}
+        </Container>
+        
+      </main>
+    </div>
+  )
+}
