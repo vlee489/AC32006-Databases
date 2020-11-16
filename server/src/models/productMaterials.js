7618c818c2ce647503fffa056fb541f1c1852105
Vincent Lee <vlee@vlee.me.uk>
2020-11-16 02:24:01 +0000

    reformat products file

--- server/src/models/productMaterials.js
+++ server/src/models/productMaterials.js
@@ -1,31 +1,31 @@
 // https://vincit.github.io/objection.js/guide/models.html
 const { Model } = require('objection');
 
-class ProductMaterials extends Model{
-    // States table name
-    static get tableName() {
-        return 'ProductMaterials';
-    }
-    
-    // Set id column
-    static get idColumn() {
-        return 'ProductMaterialID';
-    }
+class ProductMaterials extends Model {
+  // States table name
+  static get tableName() {
+    return 'ProductMaterials';
+  }
 
-    static get relationMappings() {
-        return{
-            product: {
-                relation: Model.BelongsToOneRelation, // Each entry has a relation to one entry in the other table
-                modelClass: Products, // Class that has the model we're refering to
-                join: {
-                  from: 'ProductMaterials.ProductID',
-                  to: 'Products.ProductID'
-                }
-              }
-            }
+  // Set id column
+  static get idColumn() {
+    return 'ProductMaterialID';
+  }
+
+  static get relationMappings() {
+    return {
+      product: {
+        relation: Model.BelongsToOneRelation, // Each entry has a relation to one entry in the other table
+        modelClass: Products, // Class that has the model we're refering to
+        join: {
+          from: 'ProductMaterials.ProductID',
+          to: 'Products.ProductID'
         }
+      }
+    }
+  }
 }
 
 module.exports = {
-    ProductMaterials
-  };
+  ProductMaterials
+};
