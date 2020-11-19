a8b91563478e1643569cc61abb44e16f2d5e7efc
Vincent Lee <vlee@vlee.me.uk>
2020-11-19 18:48:00 +0000

    Reformat Files

--- server/src/models/purchases.js
+++ server/src/models/purchases.js
@@ -2,29 +2,29 @@
 const { Model } = require('objection');
 
 class Purchases extends Model {
-    // States table name
-    static get tableName() {
-        return 'Purchases';
-    }
-    // Set id column
-    static get idColumn() {
-        return 'PurchaseID';
-    }
-    static get relationMappings() {
-        const { ProductPurchases } = require('./productPurchases');
-        return {
-          productPurchases: {
-            relation: Model.HasManyRelation, // Each product can be in many relations
-            modelClass: ProductPurchases,  // Class that has the model we're refering to
-            join: {
-              from: 'Purchases.PurchaseID',
-              to: 'ProductPurchases.PurchaseID'
-            }
-          }
+  // States table name
+  static get tableName() {
+    return 'Purchases';
+  }
+  // Set id column
+  static get idColumn() {
+    return 'PurchaseID';
+  }
+  static get relationMappings() {
+    const { ProductPurchases } = require('./productPurchases');
+    return {
+      productPurchases: {
+        relation: Model.HasManyRelation, // Each product can be in many relations
+        modelClass: ProductPurchases,  // Class that has the model we're refering to
+        join: {
+          from: 'Purchases.PurchaseID',
+          to: 'ProductPurchases.PurchaseID'
         }
       }
+    }
+  }
 }
 
 module.exports = {
-    Purchases
+  Purchases
 };
--- server/src/models/staffShifts.js
+++ server/src/models/staffShifts.js
@@ -1,32 +1,32 @@
 // https://vincit.github.io/objection.js/guide/models.html
 const { Model } = require('objection');
 
-class StaffShifts extends Model{
-    // States table name
-    static get tableName() {
-        return 'StaffShifts';
-    }
+class StaffShifts extends Model {
+  // States table name
+  static get tableName() {
+    return 'StaffShifts';
+  }
 
-    // Set id column
-    static get idColumn() {
-        return ['ShiftID', 'StaffID'];
-    }
+  // Set id column
+  static get idColumn() {
+    return ['ShiftID', 'StaffID'];
+  }
 
-    static get relationMappings() {
-        return {
-          shifts: {
-            relation: Model.BelongsToOneRelation,
-            modelClass: Shifts,
-            join: {
-              from: 'StaffShifts.ShiftID',
-              to: 'Shifts.ShiftID'
-            }
-          }
+  static get relationMappings() {
+    return {
+      shifts: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Shifts,
+        join: {
+          from: 'StaffShifts.ShiftID',
+          to: 'Shifts.ShiftID'
         }
       }
+    }
+  }
 
 }
 
 module.exports = {
-    StaffShifts
-  };
+  StaffShifts
+};
