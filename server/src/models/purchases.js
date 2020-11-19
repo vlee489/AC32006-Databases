74edec118c70cae7344d4d256c38f61e44dc816a
Vincent Lee <vlee@vlee.me.uk>
2020-11-19 16:31:39 +0000

    Product > productPurchases

--- server/src/models/purchases.js
+++ server/src/models/purchases.js
@@ -1,7 +1,7 @@
 // https://vincit.github.io/objection.js/guide/models.html
 const { Model } = require('objection');
 
-class Purchases extends Model{
+class Purchases extends Model {
     // States table name
     static get tableName() {
         return 'Purchases';
@@ -10,8 +10,21 @@ class Purchases extends Model{
     static get idColumn() {
         return 'PurchaseID';
     }
+    static get relationMappings() {
+        const { ProductPurchases } = require('./productPurchases');
+        return {
+          productPurchases: {
+            relation: Model.HasManyRelation, // Each product can be in many relations
+            modelClass: ProductPurchases,  // Class that has the model we're refering to
+            join: {
+              from: 'Purchases.PurchaseID',
+              to: 'ProductPurchases.PurchaseID'
+            }
+          }
+        }
+      }
 }
 
 module.exports = {
     Purchases
-  };
+};
