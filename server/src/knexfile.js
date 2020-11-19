f7ad2630b436a1005fa80f859a6d8983adb08828
Vincent Lee <vlee@vlee.me.uk>
2020-11-19 18:49:51 +0000

    reformat files

--- server/src/knexfile.js
+++ server/src/knexfile.js
@@ -1,24 +1,24 @@
 // Stored Database details
 // TODO: Move to .env file for production
 module.exports = {
-    development: {
-      client: 'mysql2',
-      useNullAsDefault: true,
-      connection: {
-        host : '127.0.0.1',
-        user : 'main',
-        password : 'password',
-        database : 'mydb'
-      }
-    },
-  
-    production: {
-      client: 'mysql2',
-      connection: {
-        host : '127.0.0.1',
-        user : 'main',
-        password : 'password',
-        database : 'mydb'
-      }
+  development: {
+    client: 'mysql2',
+    useNullAsDefault: true,
+    connection: {
+      host: '127.0.0.1',
+      user: 'main',
+      password: 'password',
+      database: 'mydb'
     }
-  };
+  },
+
+  production: {
+    client: 'mysql2',
+    connection: {
+      host: '127.0.0.1',
+      user: 'main',
+      password: 'password',
+      database: 'mydb'
+    }
+  }
+};
