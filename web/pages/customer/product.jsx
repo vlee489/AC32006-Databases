1dcaf5026e65c056b0519e83533b1f3aeeaa0f9b
Max Kelly <maxwill.kelly@gmail.com>
2020-11-24 19:40:16 +0000

    Responsive product page with react-responsive

--- web/pages/customer/product.jsx
+++ web/pages/customer/product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Cookies from 'js-cookie';
+import { useMediaQuery } from 'react-responsive';
 import { Breadcrumb, Card, Container, Button, Col, Row } from 'react-bootstrap';
 import Navigation from '../../components/navigation';
 import routes from '../../routes';
@@ -27,6 +28,44 @@ export default function Product() {
     // basket.push(product);
   }
 
+  const SplitProductInfo = () => (
+    <Row>
+      <Col>
+        <Card className={`${styles.card} my-4`}>
+          <Card.Img className={styles.cardImage} variant="top" src="https://picsum.photos/720/400" />
+        </Card>
+      </Col>
+      <Col>
+        <Card className={`${styles.card} my-4`}>
+          <Card.Body>
+            <Card.Title>{product.Name}</Card.Title>
+            <Card.Text>{`£${product.Price}`}</Card.Text>
+            <Card.Text>{product.Dimensions}</Card.Text>
+            <Button variant="primary" onClick={addToBasket}>Add to basket</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  )
+
+  const UnifiedProductInfo = () => (
+    <Row>
+      <Col>
+        <Card className={`${styles.card} my-4`}>
+          <Card.Img className={styles.cardImage} variant="top" src="https://picsum.photos/720/400" />
+          <Card.Body>
+            <Card.Title>{product.Name}</Card.Title>
+            <Card.Text>{`£${product.Price}`}</Card.Text>
+            <Card.Text>{product.Dimensions}</Card.Text>
+            <Button variant="primary" onClick={addToBasket}>Add to basket</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  )
+
+  const ProductInfo = () => useMediaQuery({ query: '(min-width: 900px)' }) ? <SplitProductInfo /> : <UnifiedProductInfo />;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -48,19 +87,7 @@ export default function Product() {
           </Col>
         </Row>
         <Container>
-          <Row>
-            <Col>
-              <Card className={`${styles.card} my-4`}>
-                <Card.Img className={styles.cardImage} variant="top" src="https://picsum.photos/720/400" />
-                <Card.Body>
-                  <Card.Title>{product.Name}</Card.Title>
-                  <Card.Text>{`£${product.Price}`}</Card.Text>
-                  <Card.Text>{product.Dimensions}</Card.Text>
-                  <Button variant="primary" onClick={addToBasket}>Add to basket</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
+          <ProductInfo />
         </Container>
       </main>
     </div>
