f27eddacf570fbbd43bb3779cfb9f0c1cbc01010
Nick De Villiers <n.devilliers1999@gmail.com>
2020-11-24 15:09:34 +0000

    Created category card. Decided on categories we stock. Using apollo now.

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -1,8 +1,34 @@
 import Head from 'next/head';
+import { Card } from 'react-bootstrap';
+
+import { useQuery } from '@apollo/client';
+import withApollo from "../libraries/apollo";
+
 import Navigation from '../components/navigation';
 import styles from '../styles/index.module.scss';
 
-export default function Browse() {
+// 1 - Shelves
+// 2 - Plants
+// 3 - Tables and Desks
+// 4 - Sofas
+// 5 - Refrigerators
+// 6 - Timber
+// 7 - Chairs
+// 8 - Bedframes
+
+const Browse = () => {
+
+  const Category = () => (
+      <Col>
+        <Card className={`${styles.category}`}>
+          <Card.Img> variant="top" src={image}</Card.Img>
+          <Card.Body>
+            <Card.Title href="">{categoryName}</Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
+  )
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,3 +43,5 @@ export default function Browse() {
     </div>
   )
 }
+
+export default withApollo({ ssr: false })(Browse);
